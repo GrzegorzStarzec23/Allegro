6bde8469fbd1604bf9d8d824c48b29f69607c3c8
Grzegorz Starzec <starzecg93@gmail.com>
2023-08-12 14:13:11 +0200

    Initial commit

---
+++ src/main/java/com/starzec/allegro/AllegroApplication.java
@@ -0,0 +1,13 @@
+package com.starzec.allegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllegroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starzec/allegro/AllegroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starzec.allegro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
